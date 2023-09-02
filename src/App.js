fa79a952283258e1f2d0f91961fc2bea44a319f4
oscar50 <oscar@whaleandjaguar.co>
2023-09-01 21:49:11 -0500

    feat: create metamask connection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import "./App.css";
+import { Col, Container, Row } from "react-bootstrap";
+import Connection from "./components/Connection";
+import "../src/styles.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>AiDataVault</h1>
+        <br></br>
+        <Container className="text-center">
+          <h1>AiDataVault</h1>
+        </Container>
+        <Container>
+          <Row>
+            <Col className="col-md-12 text-center shadow-lg border border-secondary border-2 rounded mt-4">
+              <Connection></Connection>
+            </Col>
+          </Row>
+          <Row>
+            <Col className="col-md-7 text-center shadow-lg border border-secondary border-2 rounded mt-4">
+              <h2>SimpleBox</h2>
+            </Col>
+            <Col></Col>
+            <Col className="col-md-4 text-center shadow-lg border border-secondary border-2 rounded mt-4">
+              <h2>SimpleBox</h2>
+            </Col>
+          </Row>
+        </Container>
       </header>
     </div>
   );
---
+++ src/components/Connection.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { useState } from "react";
+import { Row, Col, Button } from "react-bootstrap";
+import { ethers } from "ethers";
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+export default function Connection() {
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [userBalance, setUserBalance] = useState(null);
+  const connectwalletHandler = () => {
+    if (provider) {
+      provider.send("eth_requestAccounts", []).then(async () => {
+        await accountChangedHandler(provider.getSigner());
+      });
+    } else {
+      setErrorMessage("Please Install Metamask!!!");
+    }
+  };
+  const accountChangedHandler = async (newAccount) => {
+    const address = await newAccount.getAddress();
+    setDefaultAccount(address);
+    const balance = await newAccount.getBalance();
+    setUserBalance(ethers.utils.formatEther(balance));
+    await getuserBalance(address);
+  };
+  const getuserBalance = async (address) => {
+    const balance = await provider.getBalance(address, "latest");
+  };
+
+  return (
+    <div>
+      <Row>
+        <Col className="mt-2">
+          <h2>Connection</h2>
+          <h4 className="walletAddress">Add:{defaultAccount}</h4>
+        </Col>
+        <Col>
+          <Button
+            className="btn btn-primary mt-2 mb-2"
+            onClick={connectwalletHandler}
+            size="lg"
+          >
+            {defaultAccount ? "🦊 Connected!!" : "🦊 Connect"}
+          </Button>
+          <h3>{userBalance}</h3>
+          <h3>{errorMessage}</h3>
+        </Col>
+      </Row>
+    </div>
+  );
+}
