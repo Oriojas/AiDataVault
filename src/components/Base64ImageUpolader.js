ae560d980aea9b739a3a19f34babf43d3f27842a
oscar50 <oscar@whaleandjaguar.co>
2023-09-25 23:47:33 -0500

    feat: create chainlink connection

--- src/components/Base64ImageUpolader.js
+++ src/components/Base64ImageUpolader.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Container } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Form, Button } from "react-bootstrap";
 import { ethers } from "ethers";
 import axios from "axios";
 import lighthouse from "@lighthouse-web3/sdk";
@@ -8,7 +8,18 @@ function Base64ImageUploader() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [base64Image, setBase64Image] = useState("");
   const [uploadedImage, setUploadedImage] = useState(false);
+  const [respuestaJSON, setRespuestaJSON] = useState(null);
+  const [wallet, setWallet] = useState("");
+  const [showSuccessMessage, setShowSuccessMessage] = useState(false); // Nuevo estado
+
   const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+  useEffect(() => {
+    const storedResponse = localStorage.getItem("respuestaJSON");
+    if (storedResponse) {
+      setRespuestaJSON(JSON.parse(storedResponse));
+    }
+  }, []);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -36,7 +47,7 @@ function Base64ImageUploader() {
 
     try {
       const response = await axios.post(
-        "https://b52c-186-155-94-138.ngrok-free.app/uploadImage",
+        "https://3c1f-186-155-169-52.ngrok-free.app/uploadImage",
         {
           img: base64Image,
           signedMessage: signedMessage,
@@ -45,8 +56,45 @@ function Base64ImageUploader() {
       );
       console.log("Respuesta del servidor:", response);
       setUploadedImage(true);
+
+      localStorage.setItem("respuestaJSON", JSON.stringify(response.data));
+      setRespuestaJSON(response.data);
     } catch (error) {
       console.error("Error al enviar la imagen:", error);
+    }
+  };
+
+  const handleSubmitClick = async () => {
+    const signer = provider.getSigner();
+    console.log("Account:", await signer.getAddress());
+
+    const publicKey = await signer.getAddress();
+
+    const messageRequested = (await lighthouse.getAuthMessage(publicKey)).data
+      .message;
+    const signedMessage = await signer.signMessage(messageRequested);
+    console.log(signedMessage);
+
+    if (uploadedImage) {
+      const jsonToSend = {
+        cid: respuestaJSON.cid,
+        wallet: wallet,
+        signedMessage: signedMessage,
+        publicKey: publicKey,
+      };
+
+      try {
+        // Realiza una solicitud POST al endpoint deseado con el JSON
+        const response = await axios.post(
+          "https://3c1f-186-155-169-52.ngrok-free.app/access",
+          jsonToSend
+        );
+        console.log("Respuesta del servidor:", response);
+        // Puedes realizar acciones adicionales después de enviar el formulario
+        setShowSuccessMessage(true); // Muestra el mensaje de éxito
+      } catch (error) {
+        console.error("Error al enviar el formulario:", error);
+      }
     }
   };
 
@@ -75,7 +123,39 @@ function Base64ImageUploader() {
           Send IPFS Base64
         </button>
       )}
-      {uploadedImage && <div>Imagen subida con éxito</div>}
+      {uploadedImage && (
+        <div className="alert alert-dark">
+          Image uploaded and encrypted successfully!
+        </div>
+      )}
+      <h5 className="text-start mb-2 mt-4">
+        You can share your information with another wallet
+      </h5>
+      {uploadedImage && (
+        <Form>
+          <Form.Group className="text-start mt-4" controlId="cid">
+            <Form.Label>CID:</Form.Label>
+            <Form.Control type="text" value={respuestaJSON.cid} readOnly />
+          </Form.Group>
+          <Form.Group className="text-start" controlId="wallet">
+            <Form.Label>Wallet:</Form.Label>
+            <Form.Control
+              type="text"
+              value={wallet}
+              onChange={(e) => setWallet(e.target.value)}
+            />
+          </Form.Group>
+          <Button
+            variant="primary mt-4"
+            onClick={handleSubmitClick} // Llama a la función al hacer clic en Submit
+          >
+            Share info NFT
+          </Button>
+        </Form>
+      )}
+      {showSuccessMessage && (
+        <div className="alert alert-primary mt-3">Successfully shared!</div>
+      )}
     </div>
   );
 }
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,22 +1,97 @@
-import React from "react";
-import { Container, Row } from "react-bootstrap";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Button, Table } from "react-bootstrap";
 
 export default function Resume() {
+  const [showResponse, setShowResponse] = useState(false);
+  const [showPoweredBy, setShowPoweredBy] = useState(true);
+  const [respuestaJSON, setRespuestaJSON] = useState(
+    JSON.parse(localStorage.getItem("respuestaJSON"))
+  );
+
+  useEffect(() => {
+    // Escuchar cambios en localStorage y actualizar respuestaJSON
+    const handleStorageChange = (e) => {
+      if (e.key === "respuestaJSON") {
+        setRespuestaJSON(JSON.parse(e.newValue));
+      }
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+
+    return () => {
+      // Limpia el evento de escucha al desmontar el componente
+      window.removeEventListener("storage", handleStorageChange);
+    };
+  }, []);
+
+  const handleShowResponseClick = () => {
+    setShowResponse(true);
+    setShowPoweredBy(false);
+  };
+
+  const formatJSONToTable = (json) => {
+    return (
+      <Table striped bordered hover responsive>
+        <thead>
+          <tr>
+            <th>Key</th>
+            <th>Value</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Object.entries(json).map(([key, value]) => (
+            <tr key={key}>
+              <td className="text-sm">{key}</td>
+              <td className="text-sm">{JSON.stringify(value)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  };
+
   return (
     <div>
       <Container>
         <Row className="mt-4">
-          <h4>Powered by:</h4>
-          <img
-            src={process.env.PUBLIC_URL + "lh.png"}
-            className="img-fluid"
-            width="200"
-          ></img>
-          <p class="text-start mt-3">
-            This Dapp is powered by decentralized storage, you can save your
-            files with all the LighHouse technology that allows you to encrypt
-            as soon as your information is uploaded in a perpetual manner
-          </p>
+          {showPoweredBy && (
+            <div>
+              <h4>Powered by:</h4>
+              <img
+                src={process.env.PUBLIC_URL + "lh.png"}
+                className="img-fluid"
+                width="200"
+                alt="Lighthouse Logo"
+              />
+              <p className="text-start mt-3">
+                This Dapp is powered by decentralized storage, you can save your
+                files with all the LighHouse technology that allows you to
+                encrypt as soon as your information is uploaded in a perpetual
+                manner
+              </p>
+            </div>
+          )}
+          <Button variant="primary" onClick={handleShowResponseClick}>
+            Show Response JSON
+          </Button>
+          {showResponse && respuestaJSON ? (
+            <div>
+              <div>
+                <h6 className="mt-3">Employee Information</h6>
+                {formatJSONToTable(respuestaJSON.EmployeeInformation)}
+              </div>
+              <div>
+                <h6>Employer Information</h6>
+                {formatJSONToTable(respuestaJSON.EmployerInformation)}
+              </div>
+              <div>
+                <h6>Tax Compensation</h6>
+                {formatJSONToTable(respuestaJSON.TaxCompensation)}
+              </div>
+            </div>
+          ) : (
+            <div>No Files in local</div>
+          )}
         </Row>
       </Container>
     </div>
